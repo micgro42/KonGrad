fbd34d661054c4a8f50ada4540469b9760f8cd3d
micgro42 <micgro42@2ca1843b-cc6d-44c5-9f09-7e0e9b50d820>
2014-04-15 11:06:58 +0000

    trying to make it work without the class for the moment
    git-svn-id: svn+ssh://pool2.physik.hu-berlin.de/users/stud/micgro42/CP32014/repos/KonGrad/trunk@6 2ca1843b-cc6d-44c5-9f09-7e0e9b50d820

--- src/kongrad.hh
+++ src/kongrad.hh
@@ -3,11 +3,6 @@
 #include <vector>
 
 
-class ConGrad{
-    public:
-        
-    private:
-        void MatrixVector(vector<vector<double> > Matrix, vector<double> vecin, vector<double> vecout)
-}
+        void MatrixVector(vector< vector<double> > Matrix, vector<double> vecin, vector<double> vecout);
 
 #endif
