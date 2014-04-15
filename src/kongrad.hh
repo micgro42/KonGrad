1aa9761bbb4d54e385dcf45c637cb99a70597548
micgro42 <micgro42@2ca1843b-cc6d-44c5-9f09-7e0e9b50d820>
2014-04-15 10:44:00 +0000

    some fixes
    git-svn-id: svn+ssh://pool2.physik.hu-berlin.de/users/stud/micgro42/CP32014/repos/KonGrad/trunk@5 2ca1843b-cc6d-44c5-9f09-7e0e9b50d820

--- src/kongrad.hh
+++ src/kongrad.hh
@@ -1,11 +1,13 @@
 #ifndef KONGRAD_HH
 #define KONGRAD_HH
+#include <vector>
+
 
 class ConGrad{
     public:
         
     private:
-        void MatrixVector(vector< vector< double > > Matrix, vector< double > vecin, vector< double > vecout)
+        void MatrixVector(vector<vector<double> > Matrix, vector<double> vecin, vector<double> vecout)
 }
 
 #endif
